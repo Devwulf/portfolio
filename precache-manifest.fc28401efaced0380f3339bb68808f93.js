d741866b4b97eef37e9fda124967b6000bb51b74
Mark Malabanan <mark.malabanan24@gmail.com>
2021-01-13 18:16:41 -0500

    Updates

---
+++ precache-manifest.fc28401efaced0380f3339bb68808f93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b488721b0a6368686f010155fd9651e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8618a2a3364c7b71fd2c",
+    "url": "/portfolio/static/css/main.f7dd393f.chunk.css"
+  },
+  {
+    "revision": "162164046badd7ac792a",
+    "url": "/portfolio/static/js/2.cf20eadb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.cf20eadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8618a2a3364c7b71fd2c",
+    "url": "/portfolio/static/js/main.e2cf7d45.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "66ea9b6decd72d3cd4f933de916ba6a8",
+    "url": "/portfolio/static/media/dashboard.66ea9b6d.jpg"
+  },
+  {
+    "revision": "55dc10b03b077e6fa238add08d280ed1",
+    "url": "/portfolio/static/media/welcome-img.55dc10b0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55a8cf1a9d24333a22fcb2126b308be1.js"
+  "/portfolio/precache-manifest.fc28401efaced0380f3339bb68808f93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
